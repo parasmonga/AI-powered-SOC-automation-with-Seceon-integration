c0f4ec4e57b38baf0ae23c04c1ca108add390eed
Paras <parasmonga0410@gmail.com>
2026-07-21 15:44:50 +0530

    Fix frontend API

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,15 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-
-    baseURL: "http://127.0.0.1:8000",
-
+    baseURL: "",
     headers: {
-
-        "Content-Type": "application/json"
-
-    }
-
+        "Content-Type": "application/json",
+    },
 });
 
 export default api;
